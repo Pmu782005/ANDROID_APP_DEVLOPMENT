58678c852087d2710d6ce01ca8fb08475f81ffe7
Pmu782005 <pmeet9776@gmail.com>
2024-01-22 14:12:17 +0530

    Initial commit

--- app/src/main/java/com/example/demo/List.java
+++ app/src/main/java/com/example/demo/List.java
@@ -1,15 +1,16 @@
 package com.example.demo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class List extends AppCompatActivity {
     ListView Listview;
-    String name[]={"python","DSA","C","JAVA","python","DSA","C","JAVA"
-            ,"python","DSA","C","JAVA"};
+
 
 
     @Override
@@ -17,8 +18,14 @@ public class List extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
         Listview=findViewById(R.id.List1);
-        ArrayAdapter<String> arrayAdapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,name);
-        Listview.setAdapter(arrayAdapter);
+        String [] data={"BMW","FIAT","JAGUAR","TESLA","BMW","FIAT","JAGUAR","TESLA","BMW","FIAT","JAGUAR","TESLA"};
+        Integer [] imgid={R.drawable.bmw,R.drawable.fiat,R.drawable.jaguar,R.drawable.tesla,R.drawable.bmw,R.drawable.fiat,R.drawable.jaguar,R.drawable.tesla,R.drawable.bmw,R.drawable.fiat,R.drawable.jaguar,R.drawable.tesla};
 
+    }
+    public class customadapter extends ArrayAdapter<String>{
+
+        public customadapter(@NonNull Context context, int resource) {
+            super(context, resource);
+        }
     }
 }
